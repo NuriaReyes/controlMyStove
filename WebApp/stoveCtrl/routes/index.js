0c64fb5937876c6586d360cb1fddd805c9a31405
Nuria Reyes <nuriatonantzin@gmail.com>
2018-04-21 13:45:13 -0600

    graphical interface

--- WebApp/stoveCtrl/routes/index.js
+++ WebApp/stoveCtrl/routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Control my Stove' });
+  res.render('index', {});
 });
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/on_off', function(req, res, next) {
+  res.render('index', {});
+});
+
+router.get('/timer', function(req, res, next) {
+  res.render('index', {});
+});
+
+router.get('/regulate', function(req, res, next) {
+  res.render('index', {});
 });
 
 module.exports = router;
